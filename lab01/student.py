8fc1def5b12ec2010e72d3fa9a05bc3cdc85f654
Nazar Kyrychenko <156833036+Kiriesh45@users.noreply.github.com>
2026-09-14 14:25:16 +0000

    lab01

---
+++ lab01/student.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+number = input("What is your student number? ")
+year = input("What year are you? ")
+
+print("Name: " + name + "!")
+print("Student Number:" + number)
+print("Year:" + year)
